2fb460ffe2dc402fb09a991d98ec39304412aab7
Max Cunningham <maxzinedine@gmail.com>
2025-05-18 18:10:10 +0100

    Add MLP

--- Layer.py
+++ Layer.py
@@ -2,7 +2,7 @@ from .Neuron import Neuron
 
 
 class Layer:
-    def __init__(self, num_nodes, num_inputs, **kwargs):
+    def __init__(self, num_inputs, num_nodes, **kwargs):
         self.neurons = [Neuron(num_inputs, **kwargs) for _ in range(num_nodes)]
 
     def __call__(self, xs):
---
+++ MLP.py
@@ -0,0 +1,29 @@
+from typing import List
+
+from Neuron import NonLinear
+from .Layer import Layer
+
+
+class MLP:
+    def __init__(self, shape: List[int]):
+        self.layers = []
+        self.shape = shape
+        for i, x in enumerate(shape):
+            if i < len(shape) - 2:
+                self.layers.append(Layer(x, shape[i + 1], nonlinear=NonLinear.LeReLU))
+            elif i < len(shape) - 1:
+                self.layers.append(Layer(x, shape[i + 1]))
+
+    def __call__(self, x):
+        for layer in self.layers:
+            x = layer(x)
+        return x
+
+    def params(self):
+        return [l.params() for l in self.layers]
+
+    def __str__(self):
+        return f"MLP with shape: {self.shape}"
+
+    def __repr__(self):
+        return self.__str__()
