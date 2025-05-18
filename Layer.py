a1af3938f414605cea25fcaa76ceee9713cf2e6d
Max Cunningham <maxzinedine@gmail.com>
2025-05-18 17:58:27 +0100

    Added a layer

---
+++ Layer.py
@@ -0,0 +1,12 @@
+from .Neuron import Neuron
+
+
+class Layer:
+    def __init__(self, num_nodes, num_inputs, **kwargs):
+        self.neurons = [Neuron(num_inputs, **kwargs) for _ in range(num_nodes)]
+
+    def __call__(self, xs):
+        return [n(x) for n, x in zip(self.neurons, xs)]
+
+    def params(self):
+        return [n.params() for n in self.neurons]
