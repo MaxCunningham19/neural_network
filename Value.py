e978c089b5947101546ff3f270c3f6e0fba6e4d7
Max Cunningham <maxzinedine@gmail.com>
2025-05-18 15:36:22 +0100

    Add parents

--- Value.py
+++ Value.py
@@ -1,11 +1,12 @@
-from typing import Union
+from typing import List, Union
 
 
 class Value:
-    def __init__(self, data: int | float, label="", _op=""):
+    def __init__(self, data: int | float, label="", _op="", parents=[]):
         self.data = data
         self.label = label
         self._op = _op
+        self.parents: List[Value] = parents
 
     def __str__(self):
         return f"{self.label if self.label != ""  else"Value"}(data={self.data})"
@@ -16,15 +17,15 @@ class Value:
     def __add__(self, other: Union["Value", int, float]):
         other = other if isinstance(other, Value) else Value(other)
         n = self.data + other.data
-        return Value(n, _op="+")
+        return Value(n, _op="+", parents=[self, other])
 
     def __radd__(self, other: Union["Value", int, float]):
         return self + other
 
     def __sub__(self, other: Union["Value", int, float]):
-        tmp = self + (other * -1)
-        tmp._op = "-"
-        return tmp
+        other = other if isinstance(other, Value) else Value(other)
+        n = self.data - other.data
+        return Value(n, _op="-", parents=[self, other])
 
     def __rsub__(self, other: Union["Value", int, float]):
         return self - other
@@ -32,16 +33,15 @@ class Value:
     def __mul__(self, other: Union["Value", int, float]):
         other = other if isinstance(other, Value) else Value(other)
         n = self.data * other.data
-        return Value(n, _op="*")
+        return Value(n, _op="*", parents=[self, other])
 
     def __rmul__(self, other: Union["Value", int, float]):
         return self * other
 
     def __truediv__(self, other: Union["Value", int, float]):
         other = other if isinstance(other, Value) else Value(other)
-        tmp = self * (other**-1)
-        tmp._op = "/"
-        return tmp
+        n = self.data / other.data
+        return Value(n, _op="/", parents=[self, other])
 
     def __rtruediv__(self, other: Union["Value", int, float]):
         return self / other
@@ -49,7 +49,7 @@ class Value:
     def __pow__(self, other: Union["Value", int, float]):
         other = other if isinstance(other, Value) else Value(other)
         n = self.data**other.data
-        return Value(n, _op="^")
+        return Value(n, _op="^", parents=[self, other])
 
     def __rpow__(self, other: Union["Value", int, float]):
         return self**other
